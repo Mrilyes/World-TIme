2f34d40258bb5554182ce055ff0a7f4f23958d7e
Mrilyes <ilyesghlila1@gmail.com>
2021-12-05 14:56:52 +0100

    initial commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Athens', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'Africa/Tunis', location: 'Tunis', flag: 'tunis.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+    WorldTime(url: 'Asia/Riyadh', location: 'Riyadh', flag: 'saudi.png'),
+  ];
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    Navigator.pop(context, {
+      'location': instance.location,
+      'time': instance.time,
+      'flag': instance.flag,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: ListView.builder(
+          // listView control allows you to display a list of items with item text and, optionally, an icon to identify the type of item
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+              child: Card(
+                child: ListTile(
+                  // list title putting the flag beside the location
+                  onTap: () {
+                    updateTime(index);
+                    // print(locations[index].url);
+                  },
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    // leading mean the image came before the location
+                    backgroundImage:
+                        AssetImage('assets/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
---
+++ lib/services/world_time.dart
@@ -0,0 +1,38 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+  String location; // location name for UI
+  late String time; // the time in that location
+  String flag; // url to an asset flag icon
+  String url; // location url for api endpoint
+  late bool isDaytime; // true or false if daytime or not
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+    // make the request
+    Response response =
+        await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+    Map data = jsonDecode(response.body);
+
+    // get properties from json
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    print(now);
+
+    // set the time property
+
+    isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
+    time = DateFormat.jm().format(now);
+    // the time is gonna look more easy to read thanks to intl package
+  }
+}
+
+// WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
